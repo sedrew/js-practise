bcc22284d8edab25a2adf12db9f475d7a76001df
kdanet <kdanet1822@gmail.com>
2019-02-28 20:51:37 +0300

    Голову сломал, но реализовал RLE шифрование

--- assets/123/rle.js
+++ assets/123/rle.js
@@ -11,6 +11,25 @@
  * @param  {string} input
  * @return {string}
  */
-function rle(input) {
 
+
+
+
+var str = "AAABBCQ";
+var pattern = /(.)\1*/g;
+var arr = str.match(pattern);
+var arr3 = []
+console.log(arr);
+for (var i = 0; i < arr.length; i++) {
+    str1 = arr[i];
+    var n = str1.length;
+    console.log(arr[i], n);
+    var arr2 = str1.split("")
+    console.log(arr2);
+    arr3.push(arr2[0], n);
+    console.log(arr3);
 }
+var str2 = arr3.join("");
+console.log(str2);
+var str3 = str2.replace(/1/g, "");
+console.log(str3);
