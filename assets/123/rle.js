a8c88f4a2efb70108fa35df3e7e44427dcac2e03
kdanet <kdanet1822@gmail.com>
2019-02-28 20:54:59 +0300

    Свернул в функцию

--- assets/123/rle.js
+++ assets/123/rle.js
@@ -15,21 +15,27 @@
 
 
 
-var str = "AAABBCQ";
-var pattern = /(.)\1*/g;
-var arr = str.match(pattern);
-var arr3 = []
-console.log(arr);
-for (var i = 0; i < arr.length; i++) {
-    str1 = arr[i];
-    var n = str1.length;
-    console.log(arr[i], n);
-    var arr2 = str1.split("")
-    console.log(arr2);
-    arr3.push(arr2[0], n);
-    console.log(arr3);
+
+function rle(str) {
+    console.log(str);
+    var pattern = /(.)\1*/g;
+    var arr = str.match(pattern);
+    var arr3 = []
+    // console.log(arr);
+    for (var i = 0; i < arr.length; i++) {
+        str1 = arr[i];
+        var n = str1.length;
+        // console.log(arr[i], n);
+        var arr2 = str1.split("")
+        // console.log(arr2);
+        arr3.push(arr2[0], n);
+        // console.log(arr3);
+    }
+    var str2 = arr3.join("");
+    // console.log(str2);
+    var str3 = str2.replace(/1/g, "");
+    console.log(str3);
 }
-var str2 = arr3.join("");
-console.log(str2);
-var str3 = str2.replace(/1/g, "");
-console.log(str3);
+
+var str = "AAAAAABBCQ";
+rle(str);
